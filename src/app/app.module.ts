b799bac9be7972e7aec2c8e80f2c945d0411c57c
DrCalx <matthew.duguay@gmail.com>
2018-02-24 15:36:30 -0500

    Got rid of data component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BlotterComponent } from './blotter/blotter.component';
 import { TicketComponent } from './ticket/ticket.component';
-import { HoldingsComponent } from './holdings/holdings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlotterComponent,
     TicketComponent,
-    HoldingsComponent
   ],
   imports: [
     BrowserModule
