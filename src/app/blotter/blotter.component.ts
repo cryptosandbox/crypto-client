991d91eb1554a6e03d68b5859c907dc8e9fb3f0f
DrCalx <matthew.duguay@gmail.com>
2018-02-24 14:37:31 -0500

    More skelton

---
+++ src/app/blotter/blotter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blotter',
+  templateUrl: './blotter.component.html',
+  styleUrls: ['./blotter.component.css']
+})
+export class BlotterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
