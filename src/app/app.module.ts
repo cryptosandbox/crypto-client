5e80acc9bfdaf159175ddd2018a74c2e24b8972f
DrCalx <matthew.duguay@gmail.com>
2018-02-24 14:26:57 -0500

    skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlotterComponent } from './blotter/blotter.component';
+import { TicketComponent } from './ticket/ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlotterComponent,
+    TicketComponent
   ],
   imports: [
     BrowserModule
