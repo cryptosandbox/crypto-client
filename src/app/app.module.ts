cae153f920f67a7d2897281fb2e6bb2ad266e073
DrCalx <matthew.duguay@gmail.com>
2018-02-24 15:34:32 -0500

    Blotter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BlotterComponent } from './blotter/blotter.component';
 import { TicketComponent } from './ticket/ticket.component';
+import { HoldingsComponent } from './holdings/holdings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlotterComponent,
-    TicketComponent
+    TicketComponent,
+    HoldingsComponent
   ],
   imports: [
     BrowserModule
