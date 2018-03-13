d365cda09b4ee8a508e7dbcf18ca739ba12621e6
DrCalx <matthew.duguay@gmail.com>
2018-03-13 13:19:24 -0400

    Move timer logic to component

--- src/app/blotter/blotter.component.ts
+++ src/app/blotter/blotter.component.ts
@@ -4,6 +4,7 @@ import { Wallet } from '../wallet';
 import { CryptoService } from '../crypto.service';
 import { TicketService } from '../ticket.service';
 import { WalletService } from '../wallet.service';
+import { IntervalObservable } from "rxjs/observable/IntervalObservable";
 
 @Component({
   selector: 'app-blotter',
@@ -22,16 +23,31 @@ export class BlotterComponent implements OnInit {
     public walletService: WalletService
   ) {
   }
-
+  
   ngOnInit() {
     this.walletService.getWallet()
-      .subscribe(wallet => { 
-        if(wallet) {
-          this.wallet = wallet;
-        } else {
-          this.wallet = new Wallet('', 0);
-        }
+    .subscribe(wallet => { 
+      if(wallet) {
+        this.wallet = wallet;
+      } else {
+        this.wallet = new Wallet('', 0);
+      }
+    });
+    
+    this.cryptoService.getCoins()
+      .subscribe(coins => {
+        this.coins = coins;
       });
+
+    IntervalObservable.create(3000)
+    .subscribe(t => { 
+      this.cryptoService.getCoins()
+        .subscribe(coins => { 
+          //this.updateCoins(coins);
+          this.coins = coins;
+         });
+       });
+
   }
 
   selectCoin(coin: Coin){
@@ -41,5 +57,21 @@ export class BlotterComponent implements OnInit {
 
   getTotal() {
     return 0;
+  }
+
+  onPriceChange() {
+    this.coins.forEach(coin => this.blinkClass[coin.coin] = 'blink-red')
+  }
+
+  onLastChange(event: any) {
+    console.log(event)
+    // console.log(`Price change for ${newCoin.coin}: ${oldLast} -> ${newCoin.last}`);
+    // if (newCoin.last > oldLast) {
+    //   this.blinkClass[newCoin.coin] = 'blink-green'
+    // } else if (newCoin.last < oldLast) {
+    //   this.blinkClass[newCoin.coin] = 'blink-red'
+    // } else {
+    //   this.blinkClass[newCoin.coin] = 'blink-off'
+    // }
   }
 }
--- src/app/crypto.service.ts
+++ src/app/crypto.service.ts
@@ -8,20 +8,13 @@ import { Coin } from './coin';
 @Injectable()
 export class CryptoService {
   cryptoDataUrl = environment.API_URL + '/crypto';
-  coins: Coin[];
-  private alive: boolean;
 
   constructor(
     private http: HttpClient
-  ) { 
-    this.getCoins();
-
-    IntervalObservable.create(7000)
-      .subscribe(t => { this.getCoins() })
+  ) {
   }
 
-  getCoins() {
-    this.http.get<Coin[]>(this.cryptoDataUrl)
-      .subscribe(coins => { this.coins = coins })
+  getCoins(): Observable<Coin[]> {
+    return this.http.get<Coin[]>(this.cryptoDataUrl)
   }
 }
